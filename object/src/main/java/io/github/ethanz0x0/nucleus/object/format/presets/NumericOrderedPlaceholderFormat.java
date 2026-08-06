37589c49ef1e253a5baa2baa2057dbdce93aadda
Ethanz0x0 <ethanz0x0@outlook.com>
2026-08-06 11:05:00 +0800

    v2.0.2 Release

--- object/src/main/java/io/github/ethanz0x0/nucleus/object/format/presets/NumericOrderedPlaceholderFormat.java
+++ object/src/main/java/io/github/ethanz0x0/nucleus/object/format/presets/NumericOrderedPlaceholderFormat.java
@@ -17,7 +17,7 @@ public class NumericOrderedPlaceholderFormat extends Format {
         PlaceholderFormat.Builder builder = PlaceholderFormat.builder().placeholderSection(placeholderSection);
         int index = 0;
         for (Object replacement : replacements) {
-            builder.append(String.valueOf(index), replacement.toString());
+            builder.append(String.valueOf(index), String.valueOf(replacement));
             index ++;
         }
         placeholderFormat = builder.build();
